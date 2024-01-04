fbde160d58ee551f97ed06cf823215f932f5ad2d
Julio Nabor <julionabor@gmail.com>
2024-01-04 22:37:54 +0000

    Feat: Primeira implementação da UI

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { Weather } from '../../../../models/interfaces/weather';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
-  initialCityName: string = 'São Paulo';
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  initialCityName: string = 'Porto';
   weatherDatas!: Weather;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) {}
 
@@ -18,11 +22,26 @@ export class WeatherHomeComponent implements OnInit {
   }
 
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName).subscribe({
+    this.weatherService.getWeatherDatas(cityName)
+    .pipe(
+      takeUntil(this.destroy$)
+      )
+    .subscribe({
+      // subscrevemos na açao podendo buscar depois pelo Observable
       next: (response) => {
         response && (this.weatherDatas = response);
+        console.log(response)
       },
       error: (error) => console.log(error),
     });
+  }
+  onSubmit():void{
+    this.getWeatherDatas(this.initialCityName);
+    this.initialCityName = '';
+  }
+  // destroi o subscribe evitando memory leek
+  ngOnDestroy(): void {
+      this.destroy$.next();
+      this.destroy$.complete(); 
   }
 }
